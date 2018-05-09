23e25bf90f66c0865ebe4507018c5665c92438d5
José W. A. Castillo M <jwcastillo@gmail.com>
2018-05-09 14:59:14 -0300

    token No trabaja

--- contratos.py
+++ contratos.py
@@ -11,7 +11,7 @@ TOKEN_DECIMALS = 8
 
 # This is the script hash of the address for the owner of the token
 # This can be found in ``neo-python`` with the wallet open, use ``wallet`` command
-
+TOKEN_OWNER = b'S\xefB\xc8\xdf!^\xbeZ|z\xe8\x01\xcb\xc3\xac/\xacI)'
 
 TOKEN_CIRC_KEY = b'in_circulation'
 
@@ -44,6 +44,7 @@ GARANTIA = 5000
 GANANCIAGARANTIA = 500
 MONTOARRIENDO = 2500
 MULTA = 100
+PLATAFORMA_WALLET = b'031a6c6fbbdf02ca351745fa86b9ba5a9452d785ac4f7fc2b7548ca2a46c4fcf4a'
 
 RANKARRENDADOR = 100
 RANKARRENDATARIO = 100
@@ -68,25 +69,84 @@ def Main(operation, args):
             return 0
         arrendatario_wallet_address = args[0]
         arrendador_wallet_address = args[1]
-        return RegistrarArrendatario(arrendatario_wallet_address, arrendador_wallet_address)
+        return RegistrarArrendatario(arrendatario_wallet_address, arrendador_wallet_address, PLATAFORMA_WALLET)
+
+    elif operation == 'transferir':
+        if nargs < 2:
+            print(
+                "parametros requeridos: [arrendatario_wallet_address] [to_address]")
+            return 0
+        arrendatario_wallet_address = args[0]
+        to_address = args[1]
+        return TransferirContrato(arrendatario_wallet_address, to_address)
+
+    elif operation == 'agregar_pago':
+        if nargs < 2:
+            print(
+                "parametros requeridos: [arrendatario_wallet_address] [arrendador_wallet_address]")
+            return 0
+        arrendatario_wallet_address = args[0]
+        propiedad_id = args[1]
+        return AgregarPago(arrendatario_wallet_address, arrendador_wallet_address, MONTOARRIENDO)
+
+    elif operation == 'agregar_pago_retrasado':
+        if nargs < 2:
+            print(
+                "parametros requeridos: [arrendatario_wallet_address] [arrendador_wallet_address]")
+        return 0
+        arrendatario_wallet_address = args[0]
+        propiedad_id = args[1]
+
+        return AgregarPagoRetrasado(arrendatario_wallet_address, arrendador_wallet_address, PLATAFORMA_WALLET, MONTOARRIENDO + MULTA)
 
     else:
         return 'unknown operation'
 
 
+def FiniquitoContrato(arrendatario_wallet_address, arrendador_wallet_address, id_propiedad, monto_reparacion):
+
+    msg = concat("Finiquito del Contrato: ", arrendatario_wallet_address)
+
+
+Notify(msg)
+
+context = GetContext()
+arrendador_wallet_address = Get(context, arrendatario_wallet_address)
+if not arrendador_wallet_address:
+    Notify("El arrendatario no se encuentra registrado")
+return False
+
+Notify(arrendador_wallet_address)
+return arrendador_wallet_address
+
+
+def AgregarClausulas(clausula, arrendatario_wallet_address):
+
+    msg = concat("Se registro una nueva clausula arrendatario: " +
+                 clausula, arrendatario_wallet_address)
+
+
+Notify(msg)
+
+context = GetContext()
+Put(context, "Clausula", clausula)
+return clausula
+
+
 def ConsultaArrendatario(arrendatario_wallet_address):
     msg = concat("consulta arrendatario: ", arrendatario_wallet_address)
 
-    Notify(msg)
 
-    context = GetContext()
-    arrendador_wallet_address = Get(context, arrendatario_wallet_address)
-    if not arrendador_wallet_address:
-        Notify("El arrendatario no se encuentra registrado")
-        return False
+Notify(msg)
 
-    Notify(arrendador_wallet_address)
-    return arrendador_wallet_address
+context = GetContext()
+arrendador_wallet_address = Get(context, arrendatario_wallet_address)
+if not arrendador_wallet_address:
+    Notify("El arrendatario no se encuentra registrado")
+return False
+
+Notify(arrendador_wallet_address)
+return arrendador_wallet_address
 
 
 def RegistrarArrendatario(arrendatario_wallet_address, arrendador_wallet_address):
@@ -107,3 +167,244 @@ def RegistrarArrendatario(arrendatario_wallet_address, arrendador_wallet_address
 
     Put(context, arrendatario_wallet_address, arrendador_wallet_address)
     return True
+
+
+def LogPagosAtrasados(arrendatario_wallet_address, info):
+    context = GetContext()
+    Put(context, arrendatario_wallet_address, info)
+    return True
+
+
+def TransferirContrato(arrendatario_wallet_address, to_address):
+    msg = concat("Transferir Contrato: ", arrendatario_wallet_address)
+    Notify(msg)
+    context = GetContext()
+    arrendador_wallet_address = Get(context, arrendatario_wallet_address)
+    if not arrendador_wallet_address:
+        Notify("El arrendador no se encuentra registrado")
+        return False
+
+    if not CheckWitness(arrendador_wallet_address):
+        Notify("El arrendador no tiene cartera")
+    return False
+
+    if not len(to_address) != 34:
+        Notify(
+            "Direccion de Cartera invalida. debe poseer 34 letras")
+    return False
+
+    Put(context, arrendatario_wallet_address, to_address)
+    return True
+
+
+def BorrarArrendatario(arrendatario_wallet_address):
+    msg = concat("Borrar Arrendatario: ", arrendatario_wallet_address)
+    Notify(msg)
+    context = GetContext()
+    arrendador_wallet_address = Get(context, arrendatario_wallet_address)
+    if not arrendador_wallet_address:
+        Notify("Domain is not yet registered")
+    return False
+
+    if not CheckWitness(arrendador_wallet_address):
+        Notify("No tiene permiso para realizar esta operacion, No se puede Transferir")
+    return False
+
+    Delete(context, arrendatario_wallet_address)
+    return True
+
+
+# TOKEN
+def crowdsale_available_amount(ctx):
+    """
+    :return: int The amount of tokens left for sale in the crowdsale
+    """
+
+    in_circ = Get(ctx, TOKEN_CIRC_KEY)
+
+    available = TOKEN_TOTAL_SUPPLY - in_circ
+
+    return available
+
+
+def add_to_circulation(ctx, amount):
+    """
+    Adds an amount of token to circlulation
+    :param amount: int the amount to add to circulation
+    """
+
+    current_supply = Get(ctx, TOKEN_CIRC_KEY)
+
+    current_supply += amount
+    Put(ctx, TOKEN_CIRC_KEY, current_supply)
+    return True
+
+
+def get_circulation(ctx):
+    """
+    Get the total amount of tokens in circulation
+    :return:
+        int: Total amount in circulation
+    """
+    return Get(ctx, TOKEN_CIRC_KEY)
+
+
+OnTransfer = RegisterAction('transfer', 'addr_from', 'addr_to', 'amount')
+OnApprove = RegisterAction('approve', 'addr_from', 'addr_to', 'amount')
+
+
+def handle_nep51(ctx, operation, args):
+
+    if operation == 'name':
+        return TOKEN_NAME
+
+    elif operation == 'decimals':
+        return TOKEN_DECIMALS
+
+    elif operation == 'symbol':
+        return TOKEN_SYMBOL
+
+    elif operation == 'totalSupply':
+        return Get(ctx, TOKEN_CIRC_KEY)
+
+    elif operation == 'balanceOf':
+        if len(args) == 1:
+            return Get(ctx, args[0])
+
+    elif operation == 'transfer':
+        if len(args) == 3:
+            return do_transfer(ctx, args[0], args[1], args[2])
+
+    elif operation == 'transferFrom':
+        if len(args) == 3:
+            return do_transfer_from(ctx, args[0], args[1], args[2])
+
+    elif operation == 'approve':
+        if len(args) == 3:
+            return do_approve(ctx, args[0], args[1], args[2])
+
+    elif operation == 'allowance':
+        if len(args) == 2:
+            return do_allowance(ctx, args[0], args[1])
+
+    return False
+
+
+def do_transfer(ctx, t_from, t_to, amount):
+
+    if amount <= 0:
+        return False
+
+    if len(t_to) != 20:
+        return False
+
+    if CheckWitness(t_from):
+
+        if t_from == t_to:
+            print("transfer to self!")
+            return True
+
+        from_val = Get(ctx, t_from)
+
+        if from_val < amount:
+            print("insufficient funds")
+            return False
+
+        if from_val == amount:
+            Delete(ctx, t_from)
+
+        else:
+            difference = from_val - amount
+            Put(ctx, t_from, difference)
+
+        to_value = Get(ctx, t_to)
+
+        to_total = to_value + amount
+
+        Put(ctx, t_to, to_total)
+
+        OnTransfer(t_from, t_to, amount)
+
+        return True
+    else:
+        print("from address is not the tx sender")
+
+    return False
+
+
+def do_transfer_from(ctx, t_from, t_to, amount):
+
+    if amount <= 0:
+        return False
+
+    available_key = concat(t_from, t_to)
+
+    if len(available_key) != 40:
+        return False
+
+    available_to_to_addr = Get(ctx, available_key)
+
+    if available_to_to_addr < amount:
+        print("Insufficient funds approved")
+        return False
+
+    from_balance = Get(ctx, t_from)
+
+    if from_balance < amount:
+        print("Insufficient tokens in from balance")
+        return False
+
+    to_balance = Get(ctx, t_to)
+
+    new_from_balance = from_balance - amount
+
+    new_to_balance = to_balance + amount
+
+    Put(ctx, t_to, new_to_balance)
+    Put(ctx, t_from, new_from_balance)
+
+    print("transfer complete")
+
+    new_allowance = available_to_to_addr - amount
+
+    if new_allowance == 0:
+        print("removing all balance")
+        Delete(ctx, available_key)
+    else:
+        print("updating allowance to new allowance")
+        Put(ctx, available_key, new_allowance)
+
+    OnTransfer(t_from, t_to, amount)
+
+    return True
+
+
+def do_approve(ctx, t_owner, t_spender, amount):
+
+    if not CheckWitness(t_owner):
+        return False
+
+    if amount < 0:
+        return False
+
+    # cannot approve an amount that is
+    # currently greater than the from balance
+    if Get(ctx, t_owner) >= amount:
+
+        approval_key = concat(t_owner, t_spender)
+
+        if amount == 0:
+            Delete(ctx, approval_key)
+        else:
+            Put(ctx, approval_key, amount)
+
+        OnApprove(t_owner, t_spender, amount)
+
+        return True
+
+    return False
+
+
+def do_allowance(ctx, t_owner, t_spender):
+
+    return Get(ctx, concat(t_owner, t_spender))
